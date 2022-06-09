ba5a6d6abb26e7ec91a0c3cf15406ddb916e3a5a
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-06-08 22:01:49 -0500

    added ad to cart action and reducer

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -2,13 +2,32 @@ import { reducer } from '../utils/reducers';
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
 } from '../utils/actions';
 
 const initialState = {
   products: [],
   categories: [{ name: 'Food' }],
   currentCategory: '1',
+  cart: [
+    {
+      _id: '1',
+      name: 'Soup',
+      purchaseQuantity: 1
+    },
+    {
+      _id: '2',
+      name: 'Bread',
+      purchaseQuantity: 2
+    }
+  ],
+  cartOpen: false
 };
 
 test('UPDATE_PRODUCTS', () => {
@@ -39,4 +58,14 @@ test('UPDATE_CURRENT_CATEGORY', () => {
 
   expect(newState.currentCategory).toBe('2');
   expect(initialState.currentCategory).toBe('1');
+});
+
+test('ADD_TO_CART', () => {
+  let newState = reducer(initialState, {
+    type: ADD_TO_CART,
+    product: { purchaseQuantity: 1 }
+  });
+
+  expect(newState.cart.length).toBe(3);
+  expect(initialState.cart.length).toBe(2);
 });

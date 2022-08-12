8ba86a602ac59f696d90824009c8c258b8519ef3
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-12 02:02:47 -0500

    copied code from final version

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
-import { useStoreContext } from '../../utils/GlobalState';
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
+
   const [, dispatch] = useStoreContext();
 
   const removeFromCart = item => {
@@ -11,24 +12,29 @@ const CartItem = ({ item }) => {
       type: REMOVE_FROM_CART,
       _id: item._id
     });
+    idbPromise('cart', 'delete', { ...item });
+
   };
 
   const onChange = (e) => {
     const value = e.target.value;
-  
     if (value === '0') {
       dispatch({
         type: REMOVE_FROM_CART,
         _id: item._id
       });
+      idbPromise('cart', 'delete', { ...item });
+
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value)
       });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+
     }
-  };
+  }
 
   return (
     <div className="flex-row">
